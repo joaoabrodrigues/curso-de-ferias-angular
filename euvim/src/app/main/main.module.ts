ce422e460e6649c7d27911304c7509c5dc0b8a31
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-01-23 21:06:17 -0200

    Adding menus

--- euvim/src/app/main/main.module.ts
+++ euvim/src/app/main/main.module.ts
@@ -4,11 +4,24 @@ import { MainComponent } from './main.component';
 import { RouterModule } from '@angular/router';
 import { MainRouting } from './main.routing';
 
+import { MatSidenavModule,
+         MatButtonModule,
+         MatIconModule,
+         MatListModule, 
+         MatToolbarModule, 
+         MatCardModule } from '@angular/material';
+
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    MainRouting
+    MainRouting,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   declarations: [MainComponent]
 })
